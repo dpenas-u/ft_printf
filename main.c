3c8d9e1f34c444c924a08392377b412093069ed4
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-04-07 16:46:07 +0200

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 12:01:47 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/04/07 13:52:23 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/07 16:45:37 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ int	main(void)
 	char	c;
 	char	*s1;
 	int	i;
+	int	e;
 
-	s = "%s";
-	s1 = "amigo de";
-	i = -400;
-	c = '0';
-	i = ft_printf(s, s1);
+	s = "%p";
+	s1 = "11";
+	e = 1;
+	c = 10;
+	i = 0;
+	i = ft_printf(s, (void *)c);
 	printf("\nft: %i\n", i);
-	i = printf(s, s1);
+	i = printf(s, (void *)c);
 	printf("\nft: %i\n", i);
 }
