b7c432dada070458c529d0879f2db6d2850b87e9
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-04-08 08:48:54 +0200

    Norm OK

--- ft_printf.c
+++ ft_printf.c
@@ -6,67 +6,56 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:22:47 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/04/08 08:14:21 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/08 08:48:17 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "stdio.h"
 
 int	ft_printf(char const *str, ...)
 {
-	va_list	argptr;
+	va_list	arg;
 	int		n_char;
 
-	va_start(argptr, str);
+	va_start(arg, str);
 	n_char = 0;
 	while (*str)
 	{
-		if (*str == '%' && *(str + 1) == 'c')
+		if (*str == '%')
 		{
-			n_char += ft_putchar(va_arg(argptr, int));
-			str++;
-		}
-		else if (*str == '%' && *(str + 1) == 's')
-		{
-			ft_putstr(va_arg(argptr, char *), &n_char);
-			str++;
-		}
-		else if (*str == '%' && *(str + 1) == 'p')
-		{
-			ft_putstr("0x", &n_char);
-			ft_putpnbr_base(va_arg(argptr, long int), "0123456789abcdef", &n_char);
-			str++;
-		}
-		else if (*str == '%' && (*(str + 1) == 'd' || *(str + 1) == 'i'))
-		{
-			ft_putnbr_base(va_arg(argptr, int), "0123456789", &n_char);
-			str++;
-		}
-		else if (*str == '%' && *(str + 1) == 'u')
-		{
-			ft_putunbr(va_arg(argptr, int), "0123456789", 10, &n_char);
-			str++;
-		}
-		else if (*str == 37 && *(str + 1) == 'x')
-		{
-			ft_putunbr(va_arg(argptr, int), "0123456789abcdef", 16, &n_char);
-			str++;
-		}
-		else if (*str == '%' && *(str + 1) == 'X')
-		{
-			ft_putunbr(va_arg(argptr, int), "0123456789ABCDEF", 16, &n_char);
-			str++;
-		}
-		else if (*str == 37 && *(str + 1) == '%')
-		{
-			n_char += ft_putchar(37);
-			str++;
+			if (ft_check_arg)
+				str++;
 		}
 		else
 			n_char += ft_putchar(*str);
 		str++;
 	}
-	va_end(argptr);
+	va_end(arg);
 	return (n_char);
+}
+
+int	ft_check_arg(va_list arg, char *str, int *n_char)
+{
+	if (*(str + 1) == 'c')
+		n_char += ft_putchar(va_arg(arg, int));
+	else if (*(str + 1) == 's')
+		ft_putstr(va_arg(arg, char *), &n_char);
+	else if (*(str + 1) == 'p')
+	{
+		ft_putstr("0x", &n_char);
+		ft_putpnbr_base(va_arg(arg, long int), "0123456789abcdef", &n_char);
+	}
+	else if (*(str + 1) == 'd' || *(str + 1) == 'i')
+		ft_putnbr_base(va_arg(arg, int), "0123456789", &n_char);
+	else if (*(str + 1) == 'u')
+		ft_putunbr(va_arg(arg, int), "0123456789", 10, &n_char);
+	else if (*(str + 1) == 'x')
+		ft_putunbr(va_arg(arg, int), "0123456789abcdef", 16, &n_char);
+	else if (*(str + 1) == 'X')
+		ft_putunbr(va_arg(arg, int), "0123456789ABCDEF", 16, &n_char);
+	else if (*(str + 1) == '%')
+		n_char += ft_putchar(37);
+	else
+		return (0);
+	return (1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,19 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:12:23 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/04/08 08:25:53 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/08 08:47:48 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
 
 int		ft_printf(const char *str, ...);
+int		ft_check_arg(va_list arg, char *str, int *n_char);
 int		ft_putchar(char c);
 void	ft_putstr(char *str, int *n_char);
 void	ft_putnbr_base(int nbr, char *base, int *n_char);
