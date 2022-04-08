2428421400d87fb8aa34072e6eb09dd50cc82f0a
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-04-08 09:28:50 +0200

    All checked

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 12:19:19 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/04/07 14:51:25 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/08 09:24:57 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 int	ft_putchar(char c)
 {
-	write(1, &c, 1);
-	return (1);
+	return (write(1, &c, 1));
 }
